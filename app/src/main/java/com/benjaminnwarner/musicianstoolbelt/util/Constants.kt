98e451b89f8d639dd72109d2a3b16d4b312a0af6
Ben Warner <benjaminnwarner@gmail.com>
2019-12-12 13:18:42 -0500

    RecorderConstants

---
+++ app/src/main/java/com/benjaminnwarner/musicianstoolbelt/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.benjaminnwarner.musicianstoolbelt.util
+
+object RecorderConstants {
+    const val DEFAULT_PRE_ROLL_DURATION = 3000L
+    const val DEFAULT_RECORDING_DURATION_LIMIT = 10000
+    const val DEFAULT_NEW_RECORDING_FILENAME = "recording_temp.m4a"
+}
